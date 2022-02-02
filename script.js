8fe2de158b4af36ace43c8b5e09c69b7166475f9
vishal804 <vishal804>
2022-02-02 13:44:56 +0530

    feat: Added responsiveness

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.getElementById("sidebar").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("sidebar").style.width = "0";
+}
