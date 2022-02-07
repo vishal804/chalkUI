a351faff05428d81dffb103295f3431f32af6490
vishal804 <vishal804>
2022-02-07 13:21:08 +0530

    feat: Added Toast

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function openNav() {
 function closeNav() {
   document.getElementById("sidebar").style.width = "0";
 }
+
+function toastToggle(id) {
+  var toast = document.getElementById(id);
+  if (toast.style.display === "none") {
+    toast.style.display = "flex";
+  } else {
+    toast.style.display = "none";
+  }
+}
